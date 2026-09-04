7a31b1293c82822c5d97f11f00d582ad29f83a23
thanhtike-dev <thanhtike.dev@gmail.com>
2026-09-05 04:01:31 +0630

    feat: update footer

--- projects/pizza-menu/src/index.js
+++ projects/pizza-menu/src/index.js
@@ -121,7 +121,34 @@ function Pizza({ pizzaObj }) {
 }
 
 function Footer() {
-    return <div></div>;
+    const hours = new Date().getHours();
+    const openHour = 9;
+    const closeHour = 20;
+    const isOpen = hours >= openHour && hours <= closeHour;
+    return (
+        <footer className="footer">
+            {isOpen ? (
+                <Order closeHour={closeHour} openHour={openHour} />
+            ) : (
+                <p>
+                    We're happy to welcome you between {openHour}:00 and{' '}
+                    {closeHour}:00.
+                </p>
+            )}
+        </footer>
+    );
+}
+
+function Order({ closeHour, openHour }) {
+    return (
+        <div className="order">
+            <p>
+                We're open from {openHour}:00 to {closeHour}:00. Come visit us
+                or order online.
+            </p>
+            <button className="btn">Order</button>
+        </div>
+    );
 }
 
 // React v18
